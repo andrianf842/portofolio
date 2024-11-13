d3260f12e6e34507336a4bae8c300d7feede10ba
andrianf842 <andrianfadillah5@gmail.com>
2024-11-14 03:13:33 +0700

    tidy up

--- main.js
+++ main.js
@@ -11,6 +11,27 @@ function toggleDetail(e) {
     const detail = $(item).children(".about-exp-item-detail")
 
     
-    $(detail).slideToggle()
-    
+    $(detail).slideToggle()    
+}
+
+function onFormSubmit(e){
+    e.preventDefault()
+
+    const email = $("#inp_email")
+    const subject = $("#inp_subject")
+    const message = $("#inp_message")
+
+    if (!$(email).val()) {
+        alert("email is required")
+    }else if(!$(subject).val()){
+        alert('subject is required')
+    }else if(!$(message).val()){
+        alert("message is required")
+    }else{
+        alert("form submitted")
+        $(email).val("")
+        $(subject).val("")
+        $(message).val("")
+        
+    }
 }
